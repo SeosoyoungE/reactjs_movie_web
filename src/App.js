c8864729108b5f631deffb36b65c0a6b6a2cf024
soyoung <soyseo99@naver.com>
2023-10-21 22:50:41 +0900

    prop-types 설치

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-
+import Button from "./Button";
 
 function App() {
   return (
-    <div><h1>Welcome!</h1></div>
+    <div>
+      <h1>Welcome!</h1>
+      <Button text="Continue"/>
+    </div>
   );
 }
 
 export default App;
+
