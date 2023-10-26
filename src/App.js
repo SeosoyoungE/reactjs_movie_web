7c12e18e059cf3e47600064b285769826a6dcfc6
soyoung <soyseo99@naver.com>
2023-10-26 16:27:34 +0900

    6.2 - [값]이 변화할때만 함수 호출 useEffect(f,[값])

--- src/App.js
+++ src/App.js
@@ -4,13 +4,19 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter,setValue] = useState(0);
+  const onChange=(event)=>setKeyword(event.target.value);
+  const [keyword,setKeyword]=useState("");
   const onClick=()=>setValue((prev)=>prev+1);
   console.log('all time run')
   useEffect(()=>{
     console.log("call the API once")
   },[]);
+  useEffect(()=>{
+    console.log(keyword)
+  },[keyword])
   return (
     <div>
+      <input value={keyword} onChange={onChange} type="text" placeholder="Search"></input>
       <h1 className={styles.title}>{counter}</h1>
       <Button onClick={onClick} text="click me"/>
     </div>
