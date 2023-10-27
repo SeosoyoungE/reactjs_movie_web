132a10eee69f594b81cdaf57f6314bc635d9da38
soyoung <soyseo99@naver.com>
2023-10-27 22:04:13 +0900

    6.3 - 요약 및 정리

--- src/App.js
+++ src/App.js
@@ -7,19 +7,23 @@ function App() {
   const onChange=(event)=>setKeyword(event.target.value);
   const [keyword,setKeyword]=useState("");
   const onClick=()=>setValue((prev)=>prev+1);
+  
+  //한번만 불러옴
   useEffect(()=>{
-    console.log("once run") //한번만 불러옴
+    console.log("once run") 
   },[]);
+  //키워드가 바뀔때만 불러옴
   useEffect(()=>{ 
     if(keyword!==""){
     console.log("keyword changed")
     }
-  },[keyword]) //키워드가 바뀔때만 불러옴
+  },[keyword]) 
   useEffect(()=>{ 
     if(counter!==0){
       console.log("counter changed")
     }
   },[counter])
+  //둘 중 하나라도 키워드가 바뀔때만 불러옴
   useEffect(()=>{
     console.log("counter or keyword changed")
   },[keyword,counter])
